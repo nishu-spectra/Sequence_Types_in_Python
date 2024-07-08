fc5070bfa5f4117c8b3f95554020461de9450126
nishu-spectra <sarraf.nishu@gmail.com>
2024-07-08 22:51:10 +0545

    String in Python

---
+++ String.py
@@ -0,0 +1,34 @@
+""" Strings are the collection of characters surrounded by single quotes, double quotes or triple quotes """
+
+# creation of string in python
+str1 = 'Hello' #single quote
+print(str1)
+str2 = "World" #double quote
+print(str2)
+str3 = '''Multiline string or docstring''' #triple quote
+print (str3)
+
+#indexing in Python String
+string = "Hi"
+print(string[0])
+print(string[1])
+
+#splitting in Python String
+string = "Hello"
+print(string[0:5])
+print(string[:4])
+print(string[1:])
+print(string[2:4])
+
+#Reassigning in String
+string = "Nidhi"
+print(string)
+string= 'Nishu'
+print(string)
+
+#Deletion of String
+string = "Nidhi"
+print(string)
+del string
+# print(string)- since the string is now deleted so it will show 'string' is not defined
+
