b308ff30020b1bf437c33a1518ce66613c24d338
nishu-spectra <sarraf.nishu@gmail.com>
2024-08-30 13:24:22 +0545

    All about Lists in Python

--- Lists.py
+++ Lists.py
@@ -91,3 +91,57 @@ print(f"Truth Value= {any(my_list)}")
 # all() = returns true if all elements of the lsit is true
 my_list = [0,1,4,7]
 print(f"Truth Value= {all(my_list)}")
+
+
+
+""" Lists Comprehension"""
+#Lists comprehension offers shorter syntax when we need to create a new list based on the values of an existing list 
+
+list1 = ['apple','mango','cherry','banana','guava','melon']
+newlist = [x for x in list1 ]   # will include all the elements in list1
+print(newlist)
+
+newlist = [x for x in list1 if 'a' in x]  # will include elements having character 'a'
+print(newlist)
+
+newlist = [x for x in list1 if x=='apple']  # will include if element is equal to apple
+print(newlist)
+
+list2 = [1,2,3,4,5,6]
+newlist = [x for x in list2 if x in range(4)]
+print(newlist)
+
+
+
+"""List Copying"""
+ # 1. use copy() method
+
+list1 = [1,2,3,4]
+list2 = list1.copy()
+print(list2)
+
+# 2. use list() method 
+list1 = [1,2,3]
+list2 = list(list1)
+print(list2)
+
+# 3. use slice operator(:)
+list1 = [2,3]
+list2 = list1[:]
+print(list2)
+
+
+""" List Joining"""
+# 1. use append() function
+list1= [1,2]
+list2 = [8,7,8,6]
+for x in list2:
+    list1.append(x)
+print(list1)
+
+
+# 2. use + operator
+list1= [1,2]
+list2 = [8,6]
+newlist = list1 + list2
+print(newlist)
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-string = "Nishu"
-age = 21
-formatted_string = ("My name is %s and i am %d" %{string , age})
-print(formatted_string)
